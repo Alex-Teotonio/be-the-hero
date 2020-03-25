78bac698995599c9cbe5accbe7d46ad709895e94
Alex Barbosa Teotonio <alexteotonio29@gmail.com>
2020-03-25 20:54:55 -0300

    Back-end da aplicação Be The Hero

---
+++ backend/src/routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const OngController = require('./controllers/OngControllers');
+const IncidentController = require('./controllers/IncidentsControllers');
+const ProfileController = require('./controllers/ProfileControllers');
+const SessionController = require('./controllers/SessionControllers');
+
+const routes = express.Router();
+
+routes.post('/session', SessionController.create);
+routes.get('/ongs', OngController.index);
+routes.post('/ongs' , OngController.create);
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+
+module.exports = routes;
